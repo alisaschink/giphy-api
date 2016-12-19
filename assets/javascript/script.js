2efd806a922752c16cfa91ac888a1e164c518d68
Alisa Schink <Alisa Schink>
2016-12-18 21:58:35 -0500

    initial logic for assignment to create buttons/gifs

---
+++ assets/javascript/script.js
@@ -0,0 +1,134 @@
+    //declares array of topics
+    var topics = ["Pig", "Cat", "Dog", "Sloth"];
+
+    //function re-renders the HTML to display the appropriate content
+    function displayGifs() {
+
+      //empties any gifs in the div with id "gifs-view"
+      $("#gifs-view").empty();
+
+      //sets newItem variable to be equal to the data-name of each button
+      var newItem = $(this).data("name");
+
+      //sets giphy api url
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        newItem + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
+
+      //ajax query
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+        
+        //logs response to the console 
+        console.log(response);
+
+
+        //sets variable results equal to response.data to retrieve image info
+        var results = response.data
+
+         for (var i = 0; i < results.length; i++) {
+
+          //sets variable to create div
+          var gifDiv = $("<div>");
+
+          //sets variable to create paragraph
+          var p = $("<p>");
+
+          //sets inner text of the paragraph to the rating of the gif in results[i]
+          p.text(results[i].rating);
+
+          //sets variable to create image tag
+          var gif = $("<img>");
+
+          //sets image src
+          gif.attr("src", results[i].images.fixed_height_still.url);
+
+          //sets image data-state
+          gif.attr("data-state", "still");
+
+          //sets image class
+          gif.attr("class", "gif");
+
+          //appends the paragraph to the gifsDiv
+          $(gifDiv).append(p);
+
+          //appends the gif to the gifs div
+          $(gifDiv).append(gif);
+
+          //appends each gif to the gifs-view div
+          $("#gifs-view").append(gifDiv);
+          }
+
+
+      });  
+    };
+
+
+    //on click function for gifs to animate/unanimate
+    $(".gif").on("click", function() {
+
+      //creates a state variable for each gifs data-state
+      var state = $(this).data("state");
+
+      //if statement to animate/unanimate on each click
+      if (state === "still"){
+          //sets gif src to animated url
+          $(this).attr("src", results[i].images.fixed_height.url);
+          //changes state to animate
+          $(this).attr("data-state", "animate");
+        } else {
+          //sets gif src to still url
+          $(this).attr("src", results[i].images.fixed_height_still.url);
+            //changes state to still
+            $(this).attr("data-state", "still");
+        };
+      });
+
+
+      //function for displaying topic buttons
+      function renderButtons() {
+
+        //empties initial display of buttons so that doubles aren't created
+        $("#buttons-view").empty();
+
+        //loops through the array of topics
+        for (var i = 0; i < topics.length; i++) {
+
+          //generates buttons for each movie in the array
+          var button = $("<button>");
+          //adds a class of topic to button
+          button.addClass("topic");
+          //adds a data-attribute
+          button.attr("data-name", topics[i]);
+          //provides the initial button text
+          button.text(topics[i]);
+          //adds the button to the buttons-view div
+          $("#buttons-view").append(button);
+        }
+      }
+
+    
+
+      //on click events to add topic when button is clicked
+      $("#add-topic").on("click", function(event) {
+        event.preventDefault();
+        //grabs the input from the textbox
+        var input = $("#user-input").val().trim();
+
+        //adds topic from the textbox to topics array
+        topics.push(input);
+
+        //empties user input field
+        // $("#user-input").empty();
+
+        //runs function to create buttons
+        renderButtons();
+
+      });
+
+      //click event listeners for all elements with a class of "topic"
+      $(document).on("click", ".topic", displayGifs);
+
+      //runs function to create buttons
+      renderButtons();
