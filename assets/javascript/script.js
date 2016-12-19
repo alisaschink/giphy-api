a5a8ebe87a42ae1bf3ecee35f91f219f825132a3
Alisa Schink <Alisa Schink>
2016-12-18 23:30:02 -0500

    onclick for gifs to animate works

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -36,19 +36,25 @@
           var p = $("<p>");
 
           //sets inner text of the paragraph to the rating of the gif in results[i]
-          p.text(results[i].rating);
+          p.text("Rating: " + results[i].rating);
 
           //sets variable to create image tag
           var gif = $("<img>");
 
+          //sets image class
+          gif.attr("class", "gif");
+
           //sets image src
           gif.attr("src", results[i].images.fixed_height_still.url);
 
+          //sets image still url
+          gif.attr("data-still", results[i].images.fixed_height_still.url);
+
+          //sets image animate url
+          gif.attr("data-animate", results[i].images.fixed_height.url);
+
           //sets image data-state
           gif.attr("data-state", "still");
-
-          //sets image class
-          gif.attr("class", "gif");
 
           //appends the paragraph to the gifsDiv
           $(gifDiv).append(p);
@@ -59,32 +65,8 @@
           //appends each gif to the gifs-view div
           $("#gifs-view").append(gifDiv);
           }
-
-
       });  
     };
-
-
-    //on click function for gifs to animate/unanimate
-    $(".gif").on("click", function() {
-
-      //creates a state variable for each gifs data-state
-      var state = $(this).data("state");
-
-      //if statement to animate/unanimate on each click
-      if (state === "still"){
-          //sets gif src to animated url
-          $(this).attr("src", results[i].images.fixed_height.url);
-          //changes state to animate
-          $(this).attr("data-state", "animate");
-        } else {
-          //sets gif src to still url
-          $(this).attr("src", results[i].images.fixed_height_still.url);
-            //changes state to still
-            $(this).attr("data-state", "still");
-        };
-      });
-
 
       //function for displaying topic buttons
       function renderButtons() {
@@ -109,7 +91,6 @@
       }
 
     
-
       //on click events to add topic when button is clicked
       $("#add-topic").on("click", function(event) {
         event.preventDefault();
@@ -127,8 +108,32 @@
 
       });
 
+
+    //function for gifs to animate/unanimate
+    function animate(){
+
+      //creates a state variable for each gifs data-state
+      var state = $(this).data("state");
+
+      //if statement to animate/unanimate on each click
+      if (state === "still"){
+          //sets gif src to animated url
+          $(this).attr("src", $(this).data("animate"));
+          //changes state to animate
+          $(this).attr("data-state", "animate");
+        } else {
+          //sets gif src to still url
+          $(this).attr("src", $(this).data("still"));
+          //changes state to still
+          $(this).attr("data-state", "still");
+        };
+    };
+
       //click event listeners for all elements with a class of "topic"
       $(document).on("click", ".topic", displayGifs);
+
+      //click event listeners for all elements with a class of "gif"
+      $(document).on("click", ".gif", animate);
 
       //runs function to create buttons
       renderButtons();
